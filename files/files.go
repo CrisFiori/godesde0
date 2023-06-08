4606745a54262f578d4a69bb2224a08552880ee5
Cristian Fiori <cris.fiori90@gmail.com>
2023-06-08 12:45:40 -0300

    Modulo completo de Manejo de archivos!

--- files/files.go
+++ files/files.go
@@ -1,47 +1,60 @@
 package files
 
-import(
+import (
+	"bufio"
+	"fmt"
 	"home/cfiori/go/src/github.com/CrisFiori/godesde0/ejercicios"
 	"os"
-	//"bufio"
-	//"ioutil"
-	"fmt"
 )
 
 var filename string = "./files/txt/texto.txt"
 
-func GrabaTabla(){
+func GrabaTabla() {
 	var texto string = ejercicios.Multiplicando()
-	archivo, err:= os.Create(filename)
-	if err != nil{
-		fmt.Println("Error al crear el archivo"+err.Error())
+	archivo, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error al crear el archivo" + err.Error())
 		return
 	}
 	fmt.Fprintln(archivo, texto)
 	archivo.Close()
 }
 
-func SumaTabla(){
+func SumaTabla() {
 	var texto string = ejercicios.Multiplicando()
-	if Append(filename, texto) == false {
+	if !Append(filename, texto) {
 		fmt.Println("Error al concatenar contenido")
-	} 
+	}
 }
 
 func Append(filen string, texto string) bool {
 	arch, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil{
-		fmt.Println("Error durante el Append" +err.Error())
+	if err != nil {
+		fmt.Println("Error durante el Append" + err.Error())
 		return false
 	}
 
 	_, err = arch.WriteString(texto)
-	if err != nil{
-		fmt.Println("Error durante el WriteString"+err.Error())
+	if err != nil {
+		fmt.Println("Error durante el WriteString" + err.Error())
 		return false
 	}
 
 	arch.Close()
 	return true
 
+}
+
+func LeoArchivo() {
+	archivo, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Hubo un error leyendo el archivo" + err.Error())
+		return
+	}
+	scanner := bufio.NewScanner(archivo)
+	for scanner.Scan() {
+		registro := scanner.Text()
+		fmt.Println("»" + registro)
+	}
+	archivo.Close()
 }
--- main.go
+++ main.go
@@ -1,31 +1,29 @@
 package main
 
 import (
-	
 	"home/cfiori/go/src/github.com/CrisFiori/godesde0/files"
-//	"fmt"
-) 
-
+	//	"fmt"
+)
 
 func main() {
 	/*estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)
-	
-	
+
+
 	if os := runtime.GOOS; os =="linux" || os=="OS X."{
 		fmt.Println("Esto no es Windows, es", os)
 	}else {
 		fmt.Println("Esto es Windows")
 	}
-	
+
 	switch os:= runtime.GOOS; os {
 	case "linux":
 		fmt.Println("Esto es Linux")
 	case "darwin":
 		fmt.Println("Esto es Darwin")
 	default:
-		fmt.Printf("$s \n", os)			
+		fmt.Printf("$s \n", os)
 	}
 
 	nombre, cantidadPersonas := ejercicios.ConviertoaEntero("102")
@@ -35,7 +33,8 @@ func main() {
 	teclado.IngresoNumeros()
 	iteraciones.Interar()
 	fmt.Println(ejercicios.Multiplicando())
-	*/
+
 	files.SumaTabla()
+	*/
+	files.LeoArchivo()
 }
- 
